af4a939bb2d229dec62f1c3535cf4a6ac935870b
Maria Rico <mric@sitecore.net>
2022-03-11 16:26:15 -0600

    my notes

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+// to decorate a component you need to add at least 2 meta-data properties as a min --- selector and template
+@Component({
+  selector: "app-root",
+  template: "<h1>My-App</h1>",
+})
+export class AppComponent {}
