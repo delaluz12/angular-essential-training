6e2fd40b369d10fbbed2af1d3a4bbbfc7976730f
Maria Rico <mric@sitecore.net>
2022-03-14 11:42:09 -0500

    my notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'mw-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "mw-app",
+  templateUrl: "./app.component.html",
+  // can also be an array of string that has actual css written in it
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {}
+export class AppComponent {
+  // parent component data that want to share with child components == use @Input() decorator in component that you want to use it in
+  firstMediaItem = {
+    id: 1,
+    name: "Firebug",
+    medium: "Series",
+    category: "Science Fiction",
+    year: 2010,
+    watchedOn: 1294166565384,
+    isFavorite: false,
+  };
+
+  //add onMediaDeleteMethod from HTML template to handle the event emitter; since expecting it to return an object we can add param to the signature
+  onMediaItemDelete(mediaItem) {};
+}
--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'mw-media-item',
-  templateUrl: './media-item.component.html',
-  styleUrls: ['./media-item.component.css']
+  selector: "mw-media-item",
+  templateUrl: "./media-item.component.html",
+  styleUrls: ["./media-item.component.css"],
 })
-export class MediaItemComponent {}
+export class MediaItemComponent {
+  // create new class property for component & decorate it with @Input decorator to bind==tells angular to support property bindings of any instances of the MediaItemComponent  where the property name is mediaItem
+  @Input() mediaItem;
+
+  // @Output decortor allows exposing of event bindings on components; for example can use to provide notifications of a delete request from someone pressing the 'remove' btn on your app
+  @Output() delete = new EventEmitter();
+
+  onDelete() {
+    console.log("delete");
+    // emit event from the output decorator when onDelete method is triggered from HTMl template
+    // EventEmitters have a method called 'emit' ; this method expects an agrument that represents the data we can send back (can pass in null if we have nothing) if deleting could be helpful to have it send back the data requesting to be deleted
+    this.delete.emit(this.mediaItem);
+  }
+}
